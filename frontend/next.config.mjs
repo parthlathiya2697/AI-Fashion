2a99b6d18e25c698e404f8b74285460a351d3d89
parth <plathiya2611@gmail.com>
2024-11-19 21:30:29 +0530

    remove liniting

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Other existing configurations...
+    typescript: {
+      // ⚠️ WARNING:
+      // Setting this to true allows production builds to successfully complete
+      // even if your project has type errors.
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+  },
+  };
+  
+  export default nextConfig;
